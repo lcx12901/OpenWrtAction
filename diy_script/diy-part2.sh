cfdfda27d2780bf954576d78a5f61b5dc53af247
lcx12901 <50073681+lcx12901@users.noreply.github.com>
2022-11-06 20:57:35 +0800

    Update diy-part2.sh

--- diy_script/diy-part2.sh
+++ diy_script/diy-part2.sh
@@ -19,37 +19,6 @@ sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' package/lean/default-settings/fi
 # Temporary repair https://github.com/coolsnowwolf/lede/issues/8423
 # sed -i 's/^\s*$[(]call\sEnsureVendoredVersion/#&/' feeds/packages/utils/dockerd/Makefile
 
-mkdir -p files/root
-pushd files/root
-
-## Install oh-my-zsh
-# Clone oh-my-zsh repository
-git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
-
-# Install extra plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
-
-# Get .zshrc dotfile
-cp $GITHUB_WORKSPACE/scripts/.zshrc .
-mkdir -p files/root
-pushd files/root
-
-## Install oh-my-zsh
-# Clone oh-my-zsh repository
-git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
-
-# Install extra plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
-
-# Get .zshrc dotfile
-cp $GITHUB_WORKSPACE/diy_script/.zshrc .
-
-popd
-
 # 添加新主题
 # rm -rf ./feeds/luci/themes/luci-theme-argon
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
